81620901f5e87650e6a47ad69e1929f29f41d303
Anthony Gitter <gitter@biostat.wisc.edu>
2024-09-13 16:21:44 -0500

    Add script to intersect DisProt and UniProt IDs

---
+++ scripts/disordered_proteins.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+EXPECTED_PROTEIN_GYM = 186
+EXPECTED_DISPROT = 16695
+
+
+def main():
+    uniprot_df = pd.read_csv('assets/uniprot_idmapping_2024_09_13.tsv', sep='\t')
+    assert len(uniprot_df) == EXPECTED_PROTEIN_GYM
+
+    disprot_df = pd.read_csv('assets/DisProt_release_2024_06_with_ambiguous_evidences_with_evidences_marked_as_obsolete.tsv', sep='\t')
+    assert len(disprot_df) == EXPECTED_DISPROT
+
+    common_ids = set(uniprot_df['Entry']).intersection(set(disprot_df['acc']))
+    print(f'{len(common_ids)} of {EXPECTED_PROTEIN_GYM} unique UniProt IDs from Protein Gym are in DisProt')
+
+
+if __name__ == "__main__":
+    main()
